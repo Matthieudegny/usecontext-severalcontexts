b56684a1efa1aa3f76b15e61a2edae4e023f473e
Matthieudegny <matthieu.dev.degny@gmail.com>
2022-08-24 22:00:56 +0200

    initial commit

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
+import { ThemeContext } from '../contexts/ThemeContext';
 
-class  BookList extends Component {
+const BookList = () => {
 
-    render() { 
-        return (
-            <div className="book-list">
-                <ul>
-                    <li>the way of kinks</li>
-                    <li>the name of the wind</li>
-                    <li>the final empire</li>
-                </ul>
-            </div>
-        );
-    }
+    const { isLightTheme, light, dark} = useContext(ThemeContext);
+    const { books } = useContext(BookContext)
+    const theme = isLightTheme ? light : dark;
+
+    return ( 
+
+        <div className="book-list" style={{ color: theme.syntax, background: theme.bg}}>
+            <ul>
+                {books.map((book) => {
+                    return(
+                        <li key={book.id} style = {{ background: theme.ui }}>{book.title}</li>
+                    )
+                })}
+            </ul>
+        </div>
+     );
 }
  
-export default BookList ;
+export default BookList;
+ 
